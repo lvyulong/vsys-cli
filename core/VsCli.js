3b91df5b5f498e669e1b7f225d9b3e4511a309e9
吕玉龙 <lvyl@hrtps.com>
2019-10-24 19:37:31 +0800

    1

--- core/VsCli.js
+++ core/VsCli.js
@@ -1,42 +1,24 @@
 const program = require('commander');   //用来处理命令行参数
-
-
-
-
 const _ = require('underscore');
-
-
 const actionBase = require('./action/base');
 const actionCreate = require('./action/create');
 const actionConfig = require('./action/config');
-
 const actionPage = require('./action/page');
-
 module.exports = class VsCli {
-
     constructor(config){
         this.config = config;
         this.run();
     }
-
     run(){
         // 基础设置
         actionBase();
-
-
-
         // 配置项目
         actionConfig.config();
-
         // 创建新项目
         actionCreate();
-
         // actionCreate(this);
         //
         // actionPage(this);
-
-
-
         //
         program.parse(process.argv);
         //
@@ -44,5 +26,5 @@ module.exports = class VsCli {
         //     program.outputHelp();
         // }
     }
-
 };
+
