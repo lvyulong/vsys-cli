5e4e6a1405281fc2f6b84c6bb2036704c37ac506
吕玉龙 <lvyl@hrtps.com>
2021-03-03 13:42:28 +0800

    切换gitlab项目地址

--- config/git.js
+++ config/git.js
@@ -1,13 +1,12 @@
 module.exports = {
     system:{
-        url:'gitlab:git@git.corp.hrtps.com:lemon-fw/vue-pc#master',
+        url:'gitlab:corp.hrtps.com:lemon-fw/vue-pc#master',
         author:'lvyulong',
         project:'vue-pc',
         branch:'master',
     },
     mobile:{
         url:'gitlab:corp.hrtps.com:lemon-fw/vue-mobile#master',
-        url:'',
         author:'lvyulong',
         project:'vue-mobile',
         branch:'master',
